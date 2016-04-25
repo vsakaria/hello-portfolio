6d6ce17ebd1e7a3c689e4775f541338299ec80de
Vasco Gaspar <vvascogasparr@gmail.com>
2016-04-25 20:50:44 +0100

    Added Link component

---
+++ client/components/Types/Link.jsx
@@ -0,0 +1,23 @@
+'use strict';
+import React from 'react';
+
+export default React.createClass({
+
+  propTypes: {
+    link: React.PropTypes.string.isRequired,
+    before: React.PropTypes.string.isRequired
+  },
+
+  componentDidMount() {
+    console.log(this.props.link);
+  },
+
+  render() {
+    console.log(this.props.link);
+    return (
+      <div>
+        {this.props.before} <a href={this.props.link}>{this.props.link}</a>
+      </div>
+    );
+  }
+});
